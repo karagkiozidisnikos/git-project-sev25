dae1427c79e9c48386b04899ecf4179b06350271
nikosKaras <karagkiozidis.nikolaos@gmail.com>
2025-11-28 20:03:19 +0200

    feat: add subtraction functionality

---
+++ src/gr/aueb/sev/helloapp/SubApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.sev.helloapp;
+
+public class SubApp {
+
+    public static void main(String[] args) {
+        System.out.println(sub(12,2));
+    }
+
+    public static int sub (int a, int b) {
+        return a - b;
+    }
+}
