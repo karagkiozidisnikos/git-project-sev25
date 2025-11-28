5fbd6cf7d7b5dafd1e494f16d273ced4c0186a8a
nikosKaras <karagkiozidis.nikolaos@gmail.com>
2025-11-28 20:32:39 +0200

    feat: add printf and result var

--- src/gr/aueb/sev/helloapp/SubApp.java
+++ src/gr/aueb/sev/helloapp/SubApp.java
@@ -3,7 +3,11 @@ package gr.aueb.sev.helloapp;
 public class SubApp {
 
     public static void main(String[] args) {
-        System.out.println(sub(12,2));
+        int result = 0;
+        result = sub(12,4);
+
+        System.out.println(sub(10,5));
+        System.out.printf("Sub: %d", result);
     }
 
     public static int sub (int a, int b) {
