3fede52ca56d8ea931ae60e2476a451da0364e10
nikosKaras <karagkiozidis.nikolaos@gmail.com>
2025-11-28 19:11:23 +0200

    feat: add hello world

---
+++ src/gr/aueb/sev/helloapp/HelloWorld.java
@@ -0,0 +1,13 @@
+package gr.aueb.sev.helloapp;
+
+/**
+ * This project was created for showing how
+ * to make a git project. And then how to
+ * stage, commit and push changes to github
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
